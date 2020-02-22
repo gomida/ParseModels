8a9fc046e329d05eb98e28fb831c2013694b16e2
gomida <gomida@gmail.com>
2020-02-22 12:53:01 +0900

    tf, tflite

--- parse_tflite.py
+++ parse_tflite.py
@@ -1,20 +1,16 @@
 from tflite import Model
 from tflite.BuiltinOperator import BuiltinOperator 
 
-data = open("./mobilenet_v1_1.0_224.tflite", "rb").read()
-model = Model.Model.GetRootAsModel(data, 0)
+with open("./mobilenet_v1_1.0_224.tflite", "rb") as f:
+    model = Model.Model.GetRootAsModel(f, 0)
 
-# Function borrowed from https://github.com/apache/incubator-tvm
-def build_str_map(obj):
-    ret = {}
-    for field_name in dir(obj):
+    # Code borrowed from https://github.com/apache/incubator-tvm
+    ops = {}
+    for field_name in dir(BuiltinOperator):
         if not field_name.startswith('_'):
-            field_value = getattr(obj, field_name)
+            field_value = getattr(BuiltinOperator, field_name)
             if isinstance(field_value, int):
-                ret[field_value] = field_name
-    return ret
+                ops[field_value] = field_name
 
-builtinOps = build_str_map(BuiltinOperator)
-
-for i in range(0, model.OperatorCodesLength()):
-  print(builtinOps[model.OperatorCodes(i).BuiltinCode()])
+    for i in range(0, model.OperatorCodesLength()):
+        print(ops[model.OperatorCodes(i).BuiltinCode()])
